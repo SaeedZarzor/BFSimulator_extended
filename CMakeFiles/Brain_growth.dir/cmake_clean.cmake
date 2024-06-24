58d6108886171a94c98c87e7a95ad99df2521bbd
Saeed <saeed.zarzor@fau.de>
2024-06-24 14:05:50 +0200

    corrected parameters

---
+++ CMakeFiles/Brain_growth.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Brain_growth"
+  "Brain_growth.pdb"
+  "CMakeFiles/Brain_growth.dir/Brain_growth.cc.o"
+  "CMakeFiles/Brain_growth.dir/Brain_growth.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Brain_growth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Brain_growth.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Brain_growth.
+# This may be replaced when dependencies are built.
